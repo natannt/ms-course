4427507eb8716fc1153a95ef4017a65ac363e516
Natan Dadalt <natan.db@gmail.com>
2023-04-12 22:31:06 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/natannt/hrpayroll/services/PaymentService.java
@@ -0,0 +1,16 @@
+package com.natannt.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.natannt.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+		
+	}
+	
+
+}
